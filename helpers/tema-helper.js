69a1e9b8c6e663ac08d20ef205294aaecad9e63c
Karoline Caldeira Amorim <karoline.kalm360@gmail.com>
2024-06-16 22:49:23 +0000

    arumando a tela inicial.

--- helpers/tema-helper.js
+++ helpers/tema-helper.js
@@ -1,31 +1,30 @@
-let tema = "claro"
-
-export function trocarTema(body, botaoTema) {
-    if(localStorage.getItem("tema")){
-         tema = localStorage.getItem("tema")
+let tema ='claro'
+export function trocarTema(body,botaoTema ){
+    if (localStorage.getItem("tema")){
+        tema=localStorage.getItem("tema")
     }
+    
 
-
-    if (tema === "claro") {
+    if(tema === "claro"){
         body.classList.add("escuro")
-        localStorage.setItem("tema", "escuro")
-        botaoTema.computedStyleMap.justifyContent = "flex-end"
-    } else {
-        body.classList.remove("escuro")
-        localStorage.setItem("tema", "claro")
+        localStorage.setItem("tema","escuro")
+        botaoTema.style.justifyContent="flex-end"
+    } else{
+        body.classList.remove('escuro')
+        localStorage.setItem("tema","claro")
         botaoTema.style.justifyContent = "flex-start"
-    }
+    }   
+
 }
+export function verificarTema(body,botaoTema){
+    if(localStorage.getItem("tema")){
+        tema = localStorage.getItem('tema')
+    } 
 
-
-export function verificarTema(body, botaoTema) {
-    if (localStorage.getItem("tema")) {
-        tema = localStorage.getItem("tema")
+    if (tema === 'escuro') {
+        
+        body.classList.add('escuro')
+        botaoTema.style.justifyContent= "flex-end"
     }
 
-    if (tema === "escuro") {
-        body.classList.add("escuro")
-        botaoTema.style.justifyContent = "flex-end"
-    }
 }
-
